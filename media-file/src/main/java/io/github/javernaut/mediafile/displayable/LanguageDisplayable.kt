eb1a98bca05d6c95f96129d3d64cf98b5d757d6f
Javernaut <javernaut@gmail.com>
2024-11-10 17:41:06 +0100

    Removing unnecessary sdk version check

--- media-file/src/main/java/io/github/javernaut/mediafile/displayable/LanguageDisplayable.kt
+++ media-file/src/main/java/io/github/javernaut/mediafile/displayable/LanguageDisplayable.kt
@@ -1,18 +1,15 @@
 package io.github.javernaut.mediafile.displayable
 
-import android.os.Build
 import io.github.javernaut.mediafile.BasicStreamInfo
 import java.util.Locale
 
 val BasicStreamInfo.displayableLanguage: String?
     get() = language?.let {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val capitalizedLanguage = (Locale.forLanguageTag(language).getDisplayLanguage(Locale.US)
-                ?: language).replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+        val capitalizedLanguage = (Locale.forLanguageTag(language).getDisplayLanguage(Locale.US)
+            ?: language).replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
 
-            if (capitalizedLanguage.isNotEmpty()) {
-                return capitalizedLanguage
-            }
+        if (capitalizedLanguage.isNotEmpty()) {
+            return capitalizedLanguage
         }
         return null
     }
