f2fc90435283e022b65515034928257ff0b60be6
Javernaut <javernaut@gmail.com>
2025-04-18 15:01:43 +0200

    Targeting Java 17 and adjusting the proguard rules.

--- media-file/build.gradle.kts
+++ media-file/build.gradle.kts
@@ -39,11 +39,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     ndkVersion = "27.2.12479018"
     externalNativeBuild {
