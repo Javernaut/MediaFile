cbbe99783f4a8282e6b9352a8464e6eebfb85563
Javernaut <javernaut@gmail.com>
2026-03-14 13:55:01 +0100

    Returning the Kotlin version to the Version Catalog, so it is considered by Dependabot in the future

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,9 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.publishPlugin) apply false
 }
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+    }
+}
